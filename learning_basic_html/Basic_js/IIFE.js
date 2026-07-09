f6766ba57fe5bc990f6bfd5c5ff4ad61a3091a62
Denis Deus <deusdenis40@gmail.com>
2026-07-09 19:02:27 +0300

    Add IIFE and calculator implementation with basic arithmetic operations

---
+++ learning_basic_html/Basic_js/IIFE.js
@@ -0,0 +1,6 @@
+// This is a function expression
+() => console.log("foo");
+
+// The function expression is now an IIFE!
+// Although this one is not particularly useful of course
+(() => console.log("foo"))();
