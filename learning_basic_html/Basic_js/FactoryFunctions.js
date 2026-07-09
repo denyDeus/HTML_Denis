cc62ced34417e6fe39a790af3fc8301afe2f2109
Denis Deus <deusdenis40@gmail.com>
2026-07-09 14:53:19 +0300

    Add Closure.js with a function for creating adding functions and examples of usage

---
+++ learning_basic_html/Basic_js/FactoryFunctions.js
@@ -0,0 +1,9 @@
+function User(name) {
+    this.name = name;
+    this.discordName = "@" + name;   
+}
+
+function createUser(name) {
+    const discordName = "@" + name;
+    return { name, discordName };
+}
